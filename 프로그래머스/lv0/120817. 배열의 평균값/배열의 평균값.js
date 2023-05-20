72a25f920e0dc5b81348304be45cf44fd49d9882
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-05-20 20:37:36 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,7 +1,9 @@
 function solution(numbers) {
-    let answer = 0
-    numbers.forEach((i)=>{
-        answer += i
-    })
-    return answer/numbers.length;
+    // let answer = 0
+    // numbers.forEach((i)=>{
+    //     answer += i
+    // })
+    // return answer/numbers.length;
+    
+    return numbers.reduce((n,i,ind)=>{return n += i},0)/numbers.length
 }
