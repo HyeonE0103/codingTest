455d5c14653b64eaba367a4671af5b0d6ef0c53f
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-05-27 15:18:35 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,5 +1,9 @@
-function solution(my_string) {
-    let answer = [...my_string]
-    answer = answer.reverse()
-    return answer.join('')
+// function solution(my_string) {
+//     let answer = [...my_string]
+//     answer = answer.reverse()
+//     return answer.join('')
+// }
+
+function solution(my_string){
+    return [...my_string].reverse().join('')
 }
