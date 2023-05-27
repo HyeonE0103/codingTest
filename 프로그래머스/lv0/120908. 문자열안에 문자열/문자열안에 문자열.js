eeb31dc6744373c4d59a55235689af279989ddcc
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-05-27 17:17:45 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -1,15 +1,3 @@
-function solution(str1, str2) {
-    let o = 2
-    let n = 0
-    for(let i = 0; i<str1.length; i++){
-        if (str1[i] === str2[n]){
-            n++
-            if(n === str2.length){
-                return 1
-            }
-        }else{
-            n = 0
-        }
-    }
-    return o
+function solution(str1, str2){
+    return str1.includes(str2) ? 1 : 2
 }
