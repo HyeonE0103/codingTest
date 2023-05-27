69840bde65e1f6cef6c5b1c0a75b3274fe80ba92
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-05-27 23:31:42 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,13 +1,4 @@
 function solution(numbers) {
-    numbers = numbers.sort((a, b) => {
-  if (a > b) {
-    return 1;
-  } else if (a < b) {
-    return -1;
-  } else {
-    return 0;
-  }
-});
-    let num = numbers.length-1
-    return numbers[num] * numbers[num-1]
+    numbers.sort((a,b) => b - a)
+    return numbers[0] * numbers[1]
 }
