f99295f46fc5512cb0e34a775294d2f24cce9884
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-05-27 15:56:44 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,10 +1,10 @@
-function solution(my_string, letter) {
-    my_string = [...my_string]
-    my_string =  my_string.map(function(i){
-        if(i !== letter) return i
-    })
-    return my_string.join('')
-}
+// function solution(my_string, letter) {
+//     my_string = [...my_string]
+//     my_string =  my_string.map(function(i){
+//         if(i !== letter) return i
+//     })
+//     return my_string.join('')
+// }
 
 function solution(my_string, letter){
     return my_string.split(letter).join('')
