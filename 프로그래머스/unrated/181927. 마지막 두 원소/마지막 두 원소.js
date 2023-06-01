4860982e1086582ae05eade1c32416d596e21ad0
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-01 23:53:19 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,7 @@
+function solution(num_list) {
+    let num = num_list.length-1
+    if(num_list[num]>num_list[num-1]){num_list.push(num_list[num]-num_list[num-1])}else{
+        num_list.push(num_list[num]*2)
+    }
+    return num_list
+}
