ef99b4cd1214b14fdab1826f388c90e9595b9590
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-04 21:14:57 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,5 +1,11 @@
+// function solution(a, b) {
+//     a = String(a)
+//     b = String(b)
+//     return Number(a+b) > Number(b+a) ? Number(a+b) : Number(b+a)
+// }
+
 function solution(a, b) {
     a = String(a)
     b = String(b)
-    return Number(a+b) > Number(b+a) ? Number(a+b) : Number(b+a)
+    return Math.max(Number(a+b), Number(b+a))
 }
