e2fce619411cff8bd3c2638123210438fa808c20
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-04 20:45:09 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,11 +1,15 @@
+// function solution(my_string, is_prefix) {
+//     answer = []
+//     for(let i=1; i<=my_string.length; i++){
+//         answer.push(my_string.slice(0,i))
+// }
+//     for (let i of answer){
+//         if(is_prefix === i)
+//             return 1
+//     }
+//     return 0
+// }
+
 function solution(my_string, is_prefix) {
-    answer = []
-    for(let i=1; i<=my_string.length; i++){
-        answer.push(my_string.slice(0,i))
-}
-    for (let i of answer){
-        if(is_prefix === i)
-            return 1
-    }
-    return 0
+    return my_string.slice(0,is_prefix.length) === is_prefix ? 1 : 0
 }
