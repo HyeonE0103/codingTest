d998b7b8900b49803e4f381353e1b2b86f1defde
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-04 21:04:35 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,6 +1,10 @@
+// function solution(myString, pat) {
+//     return [...myString].map(x=>{
+//         if(x ==='A') return "B"
+//         else return "A"
+//     }).join("").includes(pat) ? 1 : 0
+// }
+
 function solution(myString, pat) {
-    return [...myString].map(x=>{
-        if(x ==='A') return "B"
-        else return "A"
-    }).join("").includes(pat) ? 1 : 0
+    return [...myString].map(x => x === "A" ? "B" : "A").join("").includes(pat) ? 1 : 0
 }
