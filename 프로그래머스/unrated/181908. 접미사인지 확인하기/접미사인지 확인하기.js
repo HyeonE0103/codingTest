677e88d527bce3471077f0b9c95aabe54fba837c
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-05 17:26:51 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,5 +1,9 @@
+// function solution(my_string, is_suffix) {
+//     my_string = my_string.split("").reverse().join("")
+//     is_suffix = is_suffix.split("").reverse().join("")
+//     return my_string.slice(0,is_suffix.length) === is_suffix ? 1 : 0
+// }
+
 function solution(my_string, is_suffix) {
-    my_string = my_string.split("").reverse().join("")
-    is_suffix = is_suffix.split("").reverse().join("")
-    return my_string.slice(0,is_suffix.length) === is_suffix ? 1 : 0
+    return my_string.slice(my_string.length - is_suffix.length) === is_suffix ? 1 : 0
 }
