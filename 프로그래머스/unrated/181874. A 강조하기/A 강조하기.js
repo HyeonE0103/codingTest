671b8395229ba5855dacd6cdd6f4e29a253fc6b0
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-08 21:50:39 +0900

    [unrated] Title: A 강조하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -1,3 +1,7 @@
+// function solution(myString) {
+//     return [...myString].reduce((acc,cur)=>cur === "a" || cur === "A"? acc+=cur.toUpperCase():acc+=cur.toLowerCase(),"")
+// }
+
 function solution(myString) {
-    return [...myString].reduce((acc,cur)=>cur === "a" || cur === "A"? acc+=cur.toUpperCase():acc+=cur.toLowerCase(),"")
+    return myString.toLowerCase().replaceAll('a',"A")
 }
