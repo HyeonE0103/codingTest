a39322a2a83c490603a1b3abe2434dda6fac5b04
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-10 19:05:40 +0900

    [unrated] Title: l로 만들기, Time: 8.14 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
@@ -1,4 +1,8 @@
 function solution(myString) {
-    s = ['a','b','c','d','e','f','g','h','i','j','k']
-    return myString.split("").reduce((acc, cur)=> s.includes(cur) ? acc+="l" : acc+=cur, "")
+    // s = ['a','b','c','d','e','f','g','h','i','j','k']
+    // return myString.split("").reduce((acc, cur)=> s.includes(cur) ? acc+="l" : acc+=cur, "")
+}
+
+function solution(myString) {
+    return myString.split("").reduce((acc, cur)=> cur < "l" ? acc+="l" : acc+=cur, "")
 }
