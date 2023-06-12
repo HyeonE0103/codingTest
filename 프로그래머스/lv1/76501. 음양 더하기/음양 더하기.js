06d5076abc8398fce0e9cec9af66e63910c60281
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-12 16:08:05 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -6,10 +6,16 @@
 //   return answer;
 // }
 
-function solution(absolutes, signs) {
-  let result = 0;
-  absolutes.map((x, i) => {
-    signs[i] ? (result += x) : (result -= x);
-  });
-  return result
-}
+// function solution(absolutes, signs) {
+//   let result = 0;
+//   absolutes.map((x, i) => {
+//     signs[i] ? (result += x) : (result -= x);
+//   });
+//   return result
+// }
+
+// function solution(absolutes, signs){
+//     return absolutes.reduce((acc,cur,i)=>signs[i]?acc+=cur:acc-=cur,0)
+// }
+
+const solution = (abs, sig) => abs.reduce((acc,cur,i)=>sig[i]?acc+=cur:acc-=cur,0)
