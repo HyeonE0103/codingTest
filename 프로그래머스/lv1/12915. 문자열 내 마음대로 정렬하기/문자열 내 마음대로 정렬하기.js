70e5afa657b3743417d1a3db7e729131712f049d
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-12 15:54:50 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 8.83 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,12 +1,12 @@
-function solution(strings, n) {
-  return strings
-    .map((x) => (x = x[n] + x))
-    .sort()
-    .map((x) => (x = x.slice(1, x.length)));
-}
-
-// function solution2(strings, n) {
-//   return strings.sort((s1, s2) =>
-//     s1[n] === s2[n] ? s1.localeCompare(s2) : s1[n].localeCompare(s2[n])
-//   );
+// function solution(strings, n) {
+//   return strings
+//     .map((x) => (x = x[n] + x))
+//     .sort()
+//     .map((x) => (x = x.slice(1, x.length)));
 // }
+
+function solution(strings, n) {
+  return strings.sort((s1, s2) =>
+    s1[n] === s2[n] ? s1.localeCompare(s2) : s1[n].localeCompare(s2[n])
+  );
+}
