586d00b4c164695146e21460b5c2452368cf76c7
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-16 12:22:05 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,4 +1,7 @@
 function solution(numbers) {
-    let num = [1,2,3,4,5,6,7,8,9]
-    return num.reduce((acc, cur) => numbers.includes(cur) ? acc: acc+=cur,0)
+    let answer = 0
+    for(let i=1; i<=9; i++){
+        if(!numbers.includes(i)) answer += i
+    }
+    return answer
 }
