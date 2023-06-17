a87b2ce917c650f6286cf5788144d995fc6dbb4e
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-17 14:18:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 96.36 ms, Memory: 56.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,16 @@
+function solution(participant, completion) {
+    // completion = completion.sort()
+    // return participant.sort().filter(x=>{
+    //     if(completion.indexOf(x) !== -1){
+    //         completion.splice(completion.indexOf(x),1)
+    //     }else return x
+    // }).join("")
+    
+    participant = participant.sort()
+    completion = completion.sort()
+    
+    for(let i=0; i<participant.length; i++){
+        if(participant[i] !== completion[i])
+            return participant[i]
+    }
+}
