852380257d662096672337136de259e21951ed9c
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-19 15:10:24 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,13 +1,8 @@
 function solution(num) {
     let turn = 0
-    return num === 1 ? 0 : check(num, turn) >= 500 ? -1 : check(num , turn)
-}
-
-function check(num, turn){
-    if(num === 1){
-        return turn
-    }else{
-        num = num % 2 ? (num*3)+1 : num/2
-        return check(num, turn + 1)
+    while(num !== 1 && turn !== 500){
+        num = num % 2 ? (num * 3) + 1 : num / 2
+        turn ++
     }
+    return num === 1 ? turn : -1
 }
