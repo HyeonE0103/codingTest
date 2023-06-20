1a10166f2acef960d4c347149688592f49ce93a8
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-20 14:22:04 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -1,17 +1,19 @@
-function solution(nums) {
-    let l = nums.length
-    let num = 0
-    let bool = true
-    for(let i=0; i<l-2; i++){
-        for(let j=i+1; j<l-1; j++){
-            for (let k=j+1; k<l; k++){
-                let sum = nums[i]+nums[j]+nums[k]
-                for (let m=2; m<=Math.floor(Math.sqrt(sum));m++)                 { if(sum%m===0) bool = false
-                }
-                if(bool)    num++
-                bool = true
+function solution(nums){
+    let num = 0;
+    let len = nums.length
+    for(let i=0; i<len-2; i++){
+        for(let j=i+1; j<len-1; j++){
+            for(let k=j+1; k<len; k++){
+                if(isPrime(nums[i]+nums[j]+nums[k])) num++
             }
         }
     }
     return num
+}
+
+function isPrime(num){
+    for(let i=2; i<=Math.sqrt(num); i++){
+        if(num % i === 0) return false;
+    }
+    return true;
 }
