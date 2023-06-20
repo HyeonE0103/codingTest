23512635461bec7898f084abeb8d840ac31af14a
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-20 14:58:16 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,28 +1,43 @@
+// function solution(s) {
+//   let words = new Map([
+//         ['zero', 0],
+//         ['one', 1],
+//         ['two', 2],
+//         ['three', 3],
+//         ['four', 4],
+//         ['five', 5],
+//         ['six', 6],
+//         ['seven', 7],
+//         ['eight', 8],
+//         ['nine', 9],
+//   ])
+  
+//   let string = "";
+//   let answer = "";
+//   for (let i = 0; i < s.length; i++) {
+//     if (!isNaN(s[i])) {
+//       answer += s[i];
+//     }else{
+//         string += s[i];
+//         if (words.get(string) !== undefined) {
+//         answer += words.get(string);
+//         string = "";
+//     }
+//     }
+//   }
+//   return Number(answer)
+// }
+
 function solution(s) {
-  let words = new Map([
-        ['zero', 0],
-        ['one', 1],
-        ['two', 2],
-        ['three', 3],
-        ['four', 4],
-        ['five', 5],
-        ['six', 6],
-        ['seven', 7],
-        ['eight', 8],
-        ['nine', 9],
-  ])
-  let string = "";
-  let answer = "";
-  for (let i = 0; i < s.length; i++) {
-    string += s[i];
-    if (!isNaN(s[i])) {
-      answer += s[i];
-      string = "";
+
+    var numArr = [0,1,2,3,4,5,6,7,8,9];
+    var wordArr = ['zero', 'one', 'two','three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+    for(var i = 0; i < numArr.length; i++){               
+        if(s.includes(wordArr[i])){            
+            s = s.replaceAll(wordArr[i], numArr[i]);
+        }
     }
-    if (words.get(string) !== undefined) {
-      answer += words.get(string);
-      string = "";
-    }
-  }
-  return Number(answer)
+
+    return Number(s);
 }
