1d9c58e7b644b4972c55112fd3fe8a792846fd7f
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-21 16:00:28 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.62 ms, Memory: 35.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -5,7 +5,6 @@ function solution(left, right) {
     }
     return answer
 }
-
 function isFactors(num){
     let n = 2
     for(let i=2; i<num; i++){
@@ -14,3 +13,16 @@ function isFactors(num){
     }
     return num != 1 ? n % 2 ? false : true : false
 }
+
+// function solution(left, right) {
+//     var answer = 0;
+//     for (let i = left; i <= right; i++) {
+//         if (Number.isInteger(Math.sqrt(i))) {
+//             answer -= i;
+//         } else {
+//             answer += i;
+//         }
+//     }
+//     return answer;
+// }
+// 제곱근이 정수면 약수의 개수가 홀수
