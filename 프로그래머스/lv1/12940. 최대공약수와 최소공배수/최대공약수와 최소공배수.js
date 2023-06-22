21c04eec5df0504bf3faf93b0300d398dfee750d
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-22 18:09:43 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,7 +1,10 @@
 function solution(n, m) {   // n < m
     let nums = [0,0]
-    for(let i=1; i<=Math.min(n,m); i++){
-        if(n%i===0 && m%i===0 && nums[0] < i) nums[0] = i
+    for(let i=Math.min(n,m); i<=i; i--){
+        if(n%i===0 && m%i===0){
+            nums[0] = i
+            break;
+        }
     }
     nums[1] = (n/nums[0]) * (m/nums[0]) * nums[0]
     return nums
