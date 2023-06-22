0125dd669ebe11a56782bc6dd70a8adaa224f87c
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-06-22 20:28:22 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,9 +1,7 @@
 function solution(n, arr1, arr2) {
-  let a, b, answer = [];
-  for (let i = 0; i < arr1.length; i++) {
-    a = arr1[i].toString(2).padStart(n, "0").split("");
-    b = arr2[i].toString(2).padStart(n, "0");
-    answer.push(a.reduce((acc,cur,i)=>Number(cur)+Number(b[i])>=1?acc+="#":acc+=" ",""))
+  let answer = []
+  for (let i = 0; i < n; i++) {
+      answer.push((arr1[i]|arr2[i]).toString(2).padStart(n,"0").replace(/1/g, "#").replace(/0/g, ' '))
   }
   return answer
 }
