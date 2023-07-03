0abaa6c3b883847154f0518e8c5f729fef419480
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-07-03 17:47:40 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.38 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,9 +1,6 @@
 function solution(arr, queries) {
-    for(let i=0; i<queries.length; i++){
-        num = arr[queries[i][0]]
-        num2 = arr[queries[i][1]]
-        arr[queries[i][0]] = num2
-        arr[queries[i][1]] = num
-    }
+   queries.forEach(([a,b]) => {
+       [arr[a],arr[b]] = [arr[b],arr[a]]
+   })
     return arr
 }
