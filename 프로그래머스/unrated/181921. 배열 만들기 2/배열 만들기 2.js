193e27c5afb8d629a5fa1613a4042364673a0689
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-07-04 16:48:04 +0900

    [unrated] Title: 배열 만들기 2, Time: 11.53 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,9 +1,8 @@
 function solution(l, r) {
     let nums = []
-    let num
-    for(let i=l; i<=r; i++){
-        num = i.toString()
-        if(!num.includes(1)&&!num.includes(2)&&!num.includes(3)&&!num.includes(4)&&!num.includes(6)&&!num.includes(7)&&!num.includes(8)&&!num.includes(9)) nums.push(i)
+    for (let i=l; i<=r; i++){
+        if(i.toString().split("").every(x=>x==="5"||x==="0"))
+            nums.push(i)
     }
     return nums.length === 0 ? [-1] : nums
 }
