317aeb6bb529132b3bb05998970c4bf0871db6f5
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-07-04 17:14:23 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,14 +1,8 @@
 function solution(n) {
     let num = [n]
-    while(true){
-        if (n === 1) return num
-        else if(n%2 === 0){
-            n = n/2
-            num.push(n)
-        }
-        else{
-            n = 3*n+1
-            num.push(n)
-        }
+    while(n > 1){
+        n = n % 2 === 0 ? n/2 : 3*n+1
+        num.push(n)
     }
+    return num
 }
