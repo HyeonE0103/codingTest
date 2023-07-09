fdc5d8f0f7bfbc0dfbf3d2fb1f79fbdbd2b90859
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-07-09 22:11:47 +0900

    [level 0] Title: 369게임, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,3 +1,7 @@
+// function solution(order) {
+//     return order.toString().split("").reduce((acc,cur)=>cur==='3' || cur ==='6' || cur === '9' ? acc+=1 : acc ,0)
+// }
+
 function solution(order) {
-    return order.toString().split("").reduce((acc,cur)=>cur==='3' || cur ==='6' || cur === '9' ? acc+=1 : acc ,0)
+    return (order + '').replace(/[0,1,2,4,5,7,8]/g, '').length
 }
