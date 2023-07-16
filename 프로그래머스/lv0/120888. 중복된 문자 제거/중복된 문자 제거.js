b7961e565965514d125beb7e905a60a520ef23c9
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-07-16 21:40:41 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,5 +1,3 @@
 function solution(my_string) {
-    result = new Set(my_string.split(""))
-    result = Array.from(result)
-    return result.join("")
+    return [...new Set(my_string)].join("")
 }
