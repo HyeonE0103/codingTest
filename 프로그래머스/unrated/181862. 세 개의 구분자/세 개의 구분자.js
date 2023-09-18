de86bdca06be85ce6f4713bc6c5ac4522ebadb61
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-09-18 23:48:14 +0900

    [unrated] Title: 세 개의 구분자, Time: 15.26 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,4 +1,4 @@
 function solution(myStr) {
-    let result = myStr.split("a").join("_").split("b").join("_").split("c").join("_").split("_").filter(x=>x!=="")
-    return result.length !== 0 ? result : ["EMPTY"]
+    let result =  myStr.split(/[a-c]/g).filter(x=>x)
+    return result.length ? result : ["EMPTY"]
 }
