eb39a589eba9acf835119433da6b8aee2ea9de8d
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-09-20 22:29:01 +0900

    [unrated] Title: 1로 만들기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -1,17 +1,11 @@
 function solution(num_list) {
     let result = 0
     for (let i=0; i<num_list.length; i++){
-        result += operation(num_list[i])
+        let num = num_list[i]
+        while(num !== 1){
+            num = num % 2 === 0 ? num/2 : (num-1)/2
+            result++
+        }
     }
     return result
-}
-
-function operation(num){
-    let answer = 0
-    while(num !== 1){
-        if(num % 2 === 0){num = num / 2}
-        else{num = (num-1)/2}
-        answer ++
-    }
-    return answer
 }
