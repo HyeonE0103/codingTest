598a58fcf6104e1f468b36e6a5b33e0b1479d3fc
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-09-21 23:32:14 +0900

    [unrated] Title: 글자 지우기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
@@ -1,7 +1,7 @@
 function solution(my_string, indices) {
     let string = my_string.split("")
     for(let i=0; i<indices.length; i++){
-        string.splice(indices[i],1,0)
+        string.splice(indices[i],1,"")
     }
-    return string.join("").split('0').join("")
+    return string.join("")
 }
