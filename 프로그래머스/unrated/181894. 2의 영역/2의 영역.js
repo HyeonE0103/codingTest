a04069aee5da529321f1f7450031a9d252a2d6f9
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-09-25 18:00:04 +0900

    [unrated] Title: 2의 영역, Time: 0.37 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.js
@@ -1,7 +1,6 @@
 function solution(arr) {
     const first = arr.indexOf(2)
     const second = arr.lastIndexOf(2)
-    
-    const sliceArr = arr.slice(first,second+1)
-    return sliceArr.length ? arr.slice(first,second+1) : [-1]
+
+    return first === -1 ? [-1] : arr.slice(first,second+1)
 }
