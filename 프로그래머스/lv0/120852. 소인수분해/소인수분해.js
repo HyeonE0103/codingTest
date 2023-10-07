33c9311b3ab0de433599f12f32a0968610339dc0
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-10-07 21:30:56 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -1,14 +1,12 @@
 function solution(n) {
-    let number = n
-    let result = new Set()
-    while(number !== 1){
-        for(let i=2; i<=number; i++){
-            if(number % i === 0){
-                number /= i
-                result.add(i)
-                break
-            }
-        }
+    let answer = []
+    let i=2
+    
+    while(i<=n){
+        if(n % i === 0){
+            answer.push(i)
+            n /= i
+        } else i++
     }
-    return Array.from(result)
+    return [...new Set(answer)]
 }
