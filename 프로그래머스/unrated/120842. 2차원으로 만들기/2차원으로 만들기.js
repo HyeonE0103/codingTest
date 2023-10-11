96771cb16351ed4c7f3ad0f4ff55623d541d05b5
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-10-11 20:07:10 +0900

    [level unrated] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,11 +1,5 @@
 function solution(num_list, n) {
-    var answer = [];
-    for(let i=0; i<num_list.length; i+=n){
-        let arr = []
-        for(let j=i; j<i+n; j++){
-            arr.push(num_list[j])
-        }
-        answer.push(arr)
-    }
+    let answer = [];
+    while(num_list.length) answer.push(num_list.splice(0,n))
     return answer
 }
