b7a042dcaa716e48f14489a248c239906a0b8f51
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-10-16 23:31:06 +0900

    [level unrated] Title: 배열 만들기 4, Time: 3.24 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -1,12 +1,22 @@
 function solution(arr) {
-    let stk = []
-    for(let i=0; i<arr.length; i++){
-        if(stk.length === 0) stk.push(arr[i])
-        else if(stk[stk.length-1] < arr[i]) stk.push(arr[i])
-        else{
-            stk.pop()
-            i--
+    var stk = [];
+
+    for(let i=0; i<arr.length; i++) {
+        if(stk.length === 0) {
+            stk.push(arr[i]);
+            continue;
         }
+
+        else if(stk[stk.length - 1] < arr[i]) {
+            stk.push(arr[i]);
+            continue;
+        }
+
+       else{
+           stk.pop();
+           i--;   
+       }
     }
-    return stk
+
+    return stk;
 }
