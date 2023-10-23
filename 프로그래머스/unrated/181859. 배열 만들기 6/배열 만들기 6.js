33ee0d65f8979c42fc0560a35ebfb6945b52059b
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-10-23 16:41:40 +0900

    [level unrated] Title: 배열 만들기 6, Time: 3.47 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -5,15 +5,14 @@ function solution(arr) {
         if(stk.length === 0){
             stk.push(arr[i])
             i++
-        }else{
-            if(stk[stk.length-1] === arr[i]){
+        }else if(stk[stk.length-1] === arr[i]){
                 stk.pop()
                 i++
-            }else{
-                stk.push(arr[i])
-                i++
-            }
+        }else{
+            stk.push(arr[i])
+            i++
         }
+        
     }
     return stk.length === 0 ? [-1] : stk
 }
