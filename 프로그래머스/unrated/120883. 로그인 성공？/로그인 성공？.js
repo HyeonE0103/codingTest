b53b5a00f3d15d69965d7466ef931c15f83099ea
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-10-26 00:21:19 +0900

    [level unrated] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.js
@@ -1,8 +1,5 @@
 function solution(id_pw, db) {
-    for(let i=0; i<db.length; i++){
-        if(id_pw[0] === db[i][0]){
-            return id_pw[1] === db[i][1] ? 'login' : 'wrong pw'
-        }
-    }
-    return 'fail'
+    const [id, pw] = id_pw
+    db = new Map(db);
+    return db.has(id) ? (db.get(id) === pw ? 'login' : 'wrong pw') : 'fail'
 }
