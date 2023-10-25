afe6ff1ad11dfd393270e069e710deec5e17387d
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-10-25 23:39:35 +0900

    [level unrated] Title: 캐릭터의 좌표, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,11 +1,14 @@
 function solution(keyinput, board) {
+    const [w,h] = [parseInt(board[0]/2), parseInt(board[1]/2)]
     let result = [0,0]
-    for(const el of keyinput){
-        if(el === 'left' && result[0] !== -parseInt(board[0]/2)) result[0]--
-        if(el === 'right' && result[0] !== parseInt(board[0]/2)) result[0]++
-        if(el === 'up' && result[1] !== parseInt(board[1]/2)) result[1]++
-        if(el === 'down' && result[1] !== parseInt(-board[1]/2)) result[1]--
-    }
     
+    for(const el of keyinput){
+        switch(el){
+            case 'left': if(result[0] > -w) result[0]--; break
+            case 'right': if(result[0] < w) result[0]++; break
+            case 'up': if(result[1] < h) result[1]++; break
+            case 'down': if(result[1] > -h) result[1]--; break
+        }
+    }
     return result
 }
