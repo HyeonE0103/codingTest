fe1916a4bf3d063edd1808f19a529a69397a81b8
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-10-31 18:20:10 +0900

    [level unrated] Title: 문자열 밀기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.js
@@ -1,7 +1,3 @@
 function solution(A, B) {
-    for(let i=0; i<A.length; i++){
-        if(A === B) return i
-        else A = A.slice(-1) + A.slice(0, -1)
-    }
-    return -1
+    return (B+B).indexOf(A)
 }
