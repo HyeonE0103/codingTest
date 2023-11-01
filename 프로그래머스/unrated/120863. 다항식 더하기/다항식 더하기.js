1e9b4eb15e64031b0efb0dcf7bd3c9846365fc8b
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-11-01 20:28:47 +0900

    [level unrated] Title: 다항식 더하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/unrated/120863. 다항식 더하기/다항식 더하기.js
@@ -2,21 +2,11 @@ function solution(polynomial) {
     let arr = polynomial.split(" + ")
     let [x, y] = [0, 0]
     
-    // for(const el of polynomial.split(' + ')){
-    //     if(el.includes('x')) x += el.length > 1 ? Number(el[0]) : 1
-    //     else y += Number(el)
-    // }
-    
-    for (let i = 0; i < arr.length; i++){
-        let last = arr[i].length
-        if(arr[i][last - 1] === 'x' && arr[i].length > 1){
-            x += Number(arr[i].slice(0,  last - 1 ))
-        } else if(arr[i][last - 1] === 'x' && arr[i].length === 1){
-            x ++
-        } else{
-            y += Number(arr[i])
-        }
+    for(const el of polynomial.split(' + ')){
+        if(el.includes('x')) x += el.length > 1 ? Number(el.slice(0, el.length-1)) : 1
+        else y += Number(el)
     }
+
     
     if(x === 0 && y === 0) return ""
     if(x === 0) return `${y}`
