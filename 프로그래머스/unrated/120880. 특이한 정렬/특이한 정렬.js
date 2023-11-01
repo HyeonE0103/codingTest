85242a3ea4aa483149ac2b6ab2b41b87eb37bdf7
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-11-01 18:23:14 +0900

    [level unrated] Title: 특이한 정렬, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/unrated/120880. 특이한 정렬/특이한 정렬.js
@@ -1,8 +1,3 @@
 function solution(numlist, n) {
-    return numlist.sort((a,b) => {
-        if (Math.abs(a-n) === Math.abs(b-n)){
-            return b - a
-        }
-        else return (Math.abs(a-n) - Math.abs(b-n))
-    })
+    return numlist.sort((a, b) => Math.abs(a-n) - Math.abs(b-n) || b-a)
 }
