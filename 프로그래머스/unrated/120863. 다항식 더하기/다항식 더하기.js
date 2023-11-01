bfca2e413ffbfbaac6ad44ebe342d03ee2f8b41f
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-11-01 20:26:27 +0900

    [level unrated] Title: 다항식 더하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/unrated/120863. 다항식 더하기/다항식 더하기.js
@@ -1,24 +1,29 @@
 function solution(polynomial) {
-  const arrPolynomial = polynomial.split(" ");
-  let arrX = arrPolynomial
-    .filter((v) => v.includes("x"))
-    .map((v) => (v.split("x")[0] ? v.split("x")[0] : "1"));
-  let num = arrPolynomial.filter((v) => +v);
-
-  if (arrX.length && num.length) {
-    arrX = arrX.reduce((a, b) => +a + +b);
-    num = num.reduce((a, b) => +a + +b);
-    if (arrX === "1") arrX = "";
-    return `${arrX + ""}x + ${num + ""}`;
-  } else {
-    if (arrX.length) {
-      arrX = arrX.reduce((a, b) => +a + +b);
-      if (arrX === "1") arrX = "";
-      return `${arrX + ""}x`;
+    let arr = polynomial.split(" + ")
+    let [x, y] = [0, 0]
+    
+    // for(const el of polynomial.split(' + ')){
+    //     if(el.includes('x')) x += el.length > 1 ? Number(el[0]) : 1
+    //     else y += Number(el)
+    // }
+    
+    for (let i = 0; i < arr.length; i++){
+        let last = arr[i].length
+        if(arr[i][last - 1] === 'x' && arr[i].length > 1){
+            x += Number(arr[i].slice(0,  last - 1 ))
+        } else if(arr[i][last - 1] === 'x' && arr[i].length === 1){
+            x ++
+        } else{
+            y += Number(arr[i])
+        }
     }
-    if (num.length) {
-      num = num.reduce((a, b) => +a + +b);
-      return num + "";
+    
+    if(x === 0 && y === 0) return ""
+    if(x === 0) return `${y}`
+    if(x === 1){
+        if(y === 0) return "x"
+        else return `x + ${y}`
     }
-  }
+    if(y === 0) return `${x}x`
+    else return `${x}x + ${y}`
 }
