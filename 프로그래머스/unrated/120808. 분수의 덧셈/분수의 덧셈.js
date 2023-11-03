16e69b6e081fc03a05d74761b14f716192fccb1f
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-11-03 18:12:30 +0900

    [level unrated] Title: 분수의 덧셈, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,13 +1,10 @@
+function gcdFn(a, b){
+    return (a%b)? gcdFn(b, a%b) : b;
+}
+
 function solution(numer1, denom1, numer2, denom2) {
-    let [numer, denom] = [numer1*denom2 + numer2*denom1, denom1*denom2]
-    let num = 2
+    let [numer, denom] = [denom1*numer2 + denom2*numer1, denom1*denom2]
+    let gcd = gcdFn(numer, denom)
     
-    while(num <= Math.min(numer, denom)){
-        if(numer % num === 0 && denom % num === 0){
-            numer /= num
-            denom /= num
-        }else num++
-    }
-    
-    return [numer, denom]
+    return [numer/gcd, denom/gcd]
 }
