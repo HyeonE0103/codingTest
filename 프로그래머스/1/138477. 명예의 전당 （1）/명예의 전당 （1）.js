b85582dbd5753baf9046ee24387cf08782ab754b
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-11-15 00:04:31 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,17 +1,8 @@
 function solution(k, score) {
-    let honor = []
     let result = []
-    for(let i=0; i<score.length; i++){
-        if(honor.length < k){
-            honor.push(score[i])
-            result.push(Math.min(...honor))
-        }
-        else{
-            if(Math.min(...honor) < score[i]){
-                honor[honor.indexOf(Math.min(...honor))] = score[i]
-                result.push(Math.min(...honor))
-            }else result.push(Math.min(...honor))
-        }
-    }
-    return result
+    return score.reduce((acc, cur) => {
+        result.push(cur);
+        result = result.sort((a, b) => b - a).slice(0, k);
+        return [...acc, Math.min(...result)];
+    }, []);
 }
