6d64f2e021a65dba2624161cec63d48c6d4a36b6
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-11-20 23:56:36 +0900

    [level 1] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,18 +1,8 @@
 function solution(cards1, cards2, goal) {
-    let num = -999
-    let c = true
-    for(let i=0; i<cards1.length; i++){
-        if(num < goal.indexOf(cards1[i]) && c) num = goal.indexOf(cards1[i])
-        else if(goal.indexOf(cards1[i]) === -1) c = false
-        else return 'No'
-    }
-    num = -999
-    c = true
-    for(let i=0; i<cards2.length; i++){
-        if(num < goal.indexOf(cards2[i]) && c) num = goal.indexOf(cards2[i])
-        else if(goal.indexOf(cards2[i]) === -1) c = false
+    for(const el of goal){
+        if(cards1[0] === el) cards1.shift();
+        else if(cards2[0] === el) cards2.shift();
         else return 'No'
     }
     return 'Yes'
-    
 }
