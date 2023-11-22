d4d2521b986df179913db187dfb45e6485a5aab0
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-11-22 23:58:24 +0900

    [level 1] Title: 과일 장수, Time: 0.06 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,8 +1,7 @@
 function solution(k, m, score) {
-    const sort = score.sort((a,b) => a-b)
+    const sort = score.sort((a,b) => b-a)
     let result = 0
-    for(let i=sort.length-1; i>=0; i-=m){
-        if(sort[i- m + 1]) result += sort[i- m + 1] * m
-    }
+    for(let i=m-1; i<sort.length; i+=m)
+        result += sort[i] * m
     return result
 }
