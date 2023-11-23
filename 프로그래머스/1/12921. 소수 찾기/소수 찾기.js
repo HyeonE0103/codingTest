933b66b6c06ca22c5635ceccbca7be7edb203b18
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-11-23 21:49:43 +0900

    [level 1] Title: 소수 찾기, Time: 27.34 ms, Memory: 45.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let arr = new Array(n+1).fill(true)
+    
+    for(let i=2; i*i<=n; i++){
+        if(arr[i]){
+            for(let j=i*i; j<=n; j+=i) arr[j] = false
+        }
+    }
+    
+    return arr.filter(el => el).length - 2
+}
