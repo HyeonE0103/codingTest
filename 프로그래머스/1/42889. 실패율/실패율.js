72ea2b71bdc26d7b5e7a33d2455386896c60d871
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-11-25 18:34:49 +0900

    [level 1] Title: 실패율, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,9 +1,10 @@
 function solution(N, stages) {
     let result = []
+    let people = stages.length
     for(let i=1; i<=N; i++){
-        let people = stages.filter(x => x>=i).length
         let num = stages.filter(x => x===i).length
         result.push([i, num/people])
+        people -= num
     }
     result.sort((a, b) => b[1] - a[1])
     return result.map(x => x[0])
