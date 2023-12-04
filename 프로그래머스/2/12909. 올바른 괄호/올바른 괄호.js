7a76a6c05bf2d34eebdd1e4270efcf8e00871f48
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-12-04 20:31:24 +0900

    [level 2] Title: 올바른 괄호, Time: 4.84 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,14 +1,8 @@
 function solution(s){
     let num = 0
     for(let el of s){
-        if(el === "("){
-            num++
-        }else{
-            num--
-            if(num<0){
-                return false
-            }
-        }
+        num += el === "(" ? 1 : -1
+        if(num < 0) return false
     }
     return num === 0 ? true : false
 }
