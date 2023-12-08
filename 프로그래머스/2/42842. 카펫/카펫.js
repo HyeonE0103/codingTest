39d46db2a5c9a7c7c51b47836b78ab7c55db401c
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-12-08 19:16:32 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,4 +1,4 @@
 function solution(brown, yellow) {
-    const x = (-4 + brown + Math.sqrt( Math.pow(4-brown, 2) - 16 * yellow))/4
-    return [x+2,yellow/x+2]
+    const x = (4+brown+Math.sqrt(Math.pow(4+brown,2)-16*(brown+yellow)))/4
+    return [x, (brown+yellow)/x]
 }
