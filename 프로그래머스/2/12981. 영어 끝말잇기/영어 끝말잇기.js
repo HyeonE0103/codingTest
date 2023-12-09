f542d926fea92b6f8126138ac37a660ce9084f19
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-12-09 19:44:56 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,14 +1,11 @@
 function solution(n, words) {
-    let num = 0
     let set = new Set([words[0]])
     
     for(let i=1; i<words.length; i++){
         if((words[i-1][words[i-1].length-1] !== words[i][0]) || set.has(words[i])){
-            num = i+1
-            break
-        }else{
-            set.add(words[i])
-        }
+            return [i%n+1, Math.floor(i/n)+1]
+        }else set.add(words[i])
+
     }
-    return num === 0 ? [0,0]: [num%n === 0 ? n : num%n, Math.ceil(num/n)]
+    return [0,0]
 }
