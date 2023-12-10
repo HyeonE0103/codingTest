392a8307ade99c5afd75e495c8b481bda7d27e7b
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-12-10 19:26:17 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -2,11 +2,8 @@ function solution(n){
     let result = 0
     
     while(n !== 0){
-        if(n % 2 === 0) n /= 2
-        else{
-            n -= 1
-            result += 1
-        }
+        result += (n % 2)
+        n = Math.floor(n/2)
     }
     return result
 }
