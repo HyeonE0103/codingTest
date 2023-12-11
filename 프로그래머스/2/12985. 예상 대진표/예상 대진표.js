d4de5ed8bd95e378d1b0fbef5b40122240135e7c
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-12-11 18:31:11 +0900

    [level 2] Title: 예상 대진표, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,9 +1,9 @@
 function solution(n,a,b){
     let answer = 0
-    while((Math.abs(a-b) !== 1) || (Math.min(a,b) % 2 !== 1)){
+    while(a !== b){
         a = Math.ceil(a/2)
         b = Math.ceil(b/2)
         answer += 1
     }
-    return answer+1
+    return answer
 }
