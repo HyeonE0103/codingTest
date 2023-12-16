3d37b94efd54d9526006bd89234a05a29178b0aa
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-12-16 18:05:57 +0900

    [level 2] Title: 귤 고르기, Time: 39.56 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476#) 
 
 ### 성능 요약
 
-메모리: 54.6 MB, 시간: 39.08 ms
+메모리: 54.5 MB, 시간: 39.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 6일 17:31:58
+2023년 12월 6일 18:5:56
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,16 +1,13 @@
 function solution(k, tangerine) {
-    let map = new Map()
     let result = 0
+    let map = new Map()
     
-    for(const el of tangerine){
-        map.set(el,map.has(el)?map.get(el)+1:1)
-    }
+    for(const el of tangerine) map.set(el, (map.get(el)||0)+1)
     map = [...map].sort((a,b)=>b[1]-a[1])
     
     for(let i=0; i<map.length; i++){
-        k-=map[i][1]
+        k -= map[i][1]
         if(k<=0) return result+1
         else result += 1
-
     }
 }
