523ac5ca2ccab3254055addffb37e6f2f35662c0
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-12-22 22:29:49 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,20 +1,22 @@
 function solution(s) {
-    let stack = []
-    let result = 0, sp = ''
+    let result = 0
+    const mapping = {')':'(', '}':'{', ']':'['}
+    
     for(let i=0; i<s.length; i++){
+        const stack = []
+        let flag = true
         for(let j=0; j<s.length; j++){
             if(s[j] === '(' || s[j] === '{' || s[j] === '[') stack.push(s[j])
             else{
                 sp = stack.pop()
-                if(!((s[j] === ')' && sp === '(') || (s[j] === '}' && sp === '{') || (s[j] === ']' && sp === '['))){
-                    stack.push(s[j])
-                    break;
+                if(sp !== mapping[s[j]]){
+                    flag = false
+                    break
                 }
             }
         }
-        if(stack.length === 0) result += 1
+        if(flag && stack.length === 0) result += 1
         s = s.slice(1) + s[0]
-        stack = []
     }
     return result
 }
