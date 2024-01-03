4c0a64480a54b719f21913379cfae2dc7f3e3022
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-01-03 22:30:44 +0900

    [level 2] Title: 행렬의 곱셈, Time: 36.99 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -2,15 +2,17 @@ function solution(arr1, arr2) {
     const result = []
     
     for(let i=0; i<arr1.length; i++){
-        const rows = []
+        const row = []
         for(let j=0; j<arr2[0].length; j++){
             let num = 0
-            for(let k=0; k<arr1[0].length; k++){
+            for(let k=0; k<arr2.length; k++){
                 num += arr1[i][k] * arr2[k][j]
             }
-            rows.push(num)
+            row.push(num)
         }
-        result.push(rows)
+        result.push(row)
     }
     return result
 }
+
+/* 한번 더 풀어보기 */
