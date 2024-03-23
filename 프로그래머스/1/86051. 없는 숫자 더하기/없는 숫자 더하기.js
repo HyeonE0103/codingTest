fa17fd8004e826a4537893ff0eadb9e6281c5347
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-03-23 23:45:31 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,4 +1,3 @@
 function solution(numbers) {
-    const num = [0,1,2,3,4,5,6,7,8,9]
-    return 45-numbers.reduce((acc,cur)=>num.includes(cur)?acc+=cur:acc,0)
+    return 45 - numbers.reduce((acc,cur)=>acc+cur,0)
 }
