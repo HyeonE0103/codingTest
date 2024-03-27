98589481a3ce4ac238382c5b21871848fc93e097
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-03-27 23:20:57 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,9 +1,4 @@
 function solution(n) {
-    const s = ['수', '박']
-    let str = ""
-    
-    for(let i=0; i<n; i++){
-        str += s[i%2]
-    }
-    return str
+    const s = '수박'.repeat(Math.floor(n/2))
+    return n % 2 === 0 ? s : s + '수'
 }
