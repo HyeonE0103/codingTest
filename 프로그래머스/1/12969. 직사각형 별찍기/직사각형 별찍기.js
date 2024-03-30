c78eb28109fd823f58fe02eea9bcad8b06536e7e
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-03-30 23:31:54 +0900

    [level 1] Title: 직사각형 별찍기, Time: 44.78 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -3,5 +3,6 @@ process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
     
-    for(let i=0; i<b; i++) console.log('*'.repeat(a))
+    const star = '*'.repeat(a)+'\n'
+    console.log(star.repeat(b))
 });
