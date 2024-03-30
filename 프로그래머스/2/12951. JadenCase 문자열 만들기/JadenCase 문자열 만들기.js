2967f2630030ef796b6ba1cf5de85a5e01f9f1a3
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-03-31 00:44:17 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,4 +1,3 @@
 function solution(s) {
-    //공백일경우 x[0]은 undefined
-    return s.split(' ').map(x =>x.charAt(0).toUpperCase() + x.slice(1).toLowerCase()).join(' ')
+    return s.toLowerCase().split(' ').map((x)=>x.slice(0,1).toUpperCase()+x.slice(1)).join(' ')
 }
