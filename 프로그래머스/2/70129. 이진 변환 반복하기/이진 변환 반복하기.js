b3734aa141df16c0e8e051480b4fa44c193de677
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-04-02 23:39:51 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 3.30 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,10 +1,11 @@
 function solution(s) {
-    let result = [0, 0]
-    while(s !== '1'){
-        result[0]++
-        result[1] += (s.match(/0/g)||[]).length
-        s = s.replace(/0/g, '').length.toString(2)
-        
+    let result = [0,0]
+    
+    while(s != '1'){
+        a = s.split('0').join('').length
+        result[1] += s.length - a
+        s = a.toString(2)
+        result[0] += 1
     }
     return result
 }
