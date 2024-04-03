61ec07b10d2056115505bb510fbc3dfd2c87312c
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-04-03 19:28:36 +0900

    [level 1] Title: 예산, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,11 @@
+function solution(d, budget) {
+    let result = 0
+    d.sort((a,b)=>a-b)
+    
+    for(let i=0; i<d.length; i++){
+        budget -= d[i]
+        if(budget >= 0 ) result += 1
+        else break
+    }
+    return result
+}
