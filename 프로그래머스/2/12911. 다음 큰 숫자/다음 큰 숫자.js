8bf42eac0410510b7cd2ef392cab1d6a8afd8d6b
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-04-06 01:16:22 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,3 +1,8 @@
-function solution(n, n2=n+1) {
-    return n.toString(2).match(/1/g).length === n2.toString(2).match(/1/g).length ? n2 : solution(n, n2+1)    
+function solution(n) {
+    const num = n.toString(2).match(/1/g).length
+    
+    while(n+1){
+        if((n+1).toString(2).match(/1/g).length === num) return n+1
+        else n = n+1
+    }
 }
