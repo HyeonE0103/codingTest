55deb13f3b428195464154302ef8505897a04d77
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-04-24 19:03:06 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,3 +1,7 @@
 function solution(array, commands) {
-    return commands.map(x=>array.slice(x[0]-1,x[1]).sort((a,b)=>a-b)[x[2]-1])
+        return commands.map(([i,j,k]) => {
+        return array
+            .slice(i-1, j)
+            .sort((a, b)=>a-b)[k-1]
+    })
 }
