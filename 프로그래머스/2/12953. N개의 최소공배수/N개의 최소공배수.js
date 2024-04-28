d6aa42fcf7769d98a908b4dd6f3f48b82430a51f
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-04-28 17:13:20 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,7 +1,16 @@
 function solution(arr) {
-    return arr.reduce((acc,cur) => acc*cur/gcd(acc, cur))
+    let result = arr[0]
+    for(let i=1; i<arr.length; i+=1){
+        result = lcm(result, arr[i])
+    }
+    
+    return result
 }
 
-function gcd(a,b){
-    return a % b === 0 ? b : gcd(b, a%b)
+function lcm(a, b){
+    return a * b / gcd(a, b)
+}
+
+function gcd(a, b){
+    return (a % b) === 0 ? b : gcd(b, a % b);
 }
--- 프로그래머스/2/12953. N개의 최소공배수/README.md
+++ 프로그래머스/2/12953. N개의 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 2] N개의 최소공배수 - 12953 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.12 ms
+메모리: 33.6 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 19:40:45
+2024년 04월 28일 17:13:19
 
 ### 문제 설명
 
