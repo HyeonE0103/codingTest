0388980424257756833c9667bbfbb817c3ecb143
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-05-01 18:22:55 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,11 +1,13 @@
 function solution(n, words) {
-    let set = new Set([words[0]])
+    let set = new Set()
+    set.add(words[0])
     
-    for(let i=1; i<words.length; i++){
-        if((words[i-1][words[i-1].length-1] !== words[i][0]) || set.has(words[i])){
+    for(let i=1; i<words.length; i+=1){
+        if(words[i][0] != words[i-1][words[i-1].length-1])
             return [i%n+1, Math.floor(i/n)+1]
-        }else set.add(words[i])
-
+        if(set.has(words[i])) return [i%n+1, Math.floor(i/n)+1]
+        else set.add(words[i])
     }
+    
     return [0,0]
 }
