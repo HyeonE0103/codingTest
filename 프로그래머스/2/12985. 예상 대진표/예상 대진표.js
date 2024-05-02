968c038992eeb95c5666f8a0482ae827e265be2b
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-05-02 20:13:40 +0900

    [level 2] Title: 예상 대진표, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,10 +1,9 @@
 function solution(n,a,b){
     let answer = 0
-    a -= 1, b -= 1
     
     while(a !== b){
-        a = Math.floor(a/2)
-        b = Math.floor(b/2)
+        a = Math.ceil(a/2)
+        b = Math.ceil(b/2)
         
         answer += 1
     }
