63b9524b451db7e2c07eaa3c7ffee00dfa580ea1
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-05-04 22:41:28 +0900

    [level 2] Title: 구명보트, Time: 10.28 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,19 +1,11 @@
 function solution(people, limit) {
-    people.sort((a,b) => (a-b));
-
-    let count = people.length;
-
-    let i = 0;
-    let j = people.length-1;
-
-    while(i < j){
-        if(people[i]+people[j] <= limit){
-            count--;
-            i++;
-            j--;
-        }else{
-            j--;
-        }
+    people.sort((a,b) => b-a)
+    let i = 0, j = people.length-1, answer = 0
+    
+    while(i<=j){
+        if(people[i] + people[j] <= limit) j-=1
+        i += 1, answer += 1
     }
-    return count;
+    
+    return answer
 }
