9f4b3e8220b46e1eecf87d64d16200c7807b96bf
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-05-05 01:20:01 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,22 +1,9 @@
 function solution(k, score) {
-    let result = [score[0]]
-    let hall = [score[0]]
+    let hall = []
     
-    for(let i=1; i<score.length; i+=1){
-        if(hall.length < k){
-            hall.push(score[i])
-            hall.sort((a,b) => b - a)
-            result.push(hall[hall.length-1])
-        }else{
-            if(hall[k-1] < score[i]){
-                hall.pop()
-                hall.push(score[i])
-                hall.sort((a,b) => b - a)
-                result.push(hall[hall.length-1])
-                
-            }else result.push(hall[k-1])
-        }
-    }
-    
-    return result
+    return score.reduce((answer, n) => {
+        hall.push(n)
+        hall = hall.sort((a, b) => b - a).slice(0, k)
+        return [...answer, Math.min(...hall)]
+    }, [])
 }
