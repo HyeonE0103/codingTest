9adb8c767b13c8e318ded3217256d34795ac9075
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-05-07 23:03:32 +0900

    [level 1] Title: 추억 점수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,3 +1,13 @@
 function solution(name, yearning, photo) {
-    return photo.map((x) => x.reduce((a, c) => a += yearning[name.indexOf(c)] ?? 0, 0))
+    let result = []
+    
+    for(let i=0; i<photo.length; i+=1){
+        let n = 0
+        for(let j=0; j<photo[i].length; j+=1){
+            const num = name.indexOf(photo[i][j])
+            if(num !== -1) n += yearning[num]
+        }
+        result.push(n)
+    }
+    return result
 }
