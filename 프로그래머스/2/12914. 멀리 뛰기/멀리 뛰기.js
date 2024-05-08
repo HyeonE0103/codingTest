dbabd7b3b4a913210604af53b5370e3b158f35c8
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-05-08 22:06:44 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,12 +1,9 @@
 function solution(n) {
-    //피보나치 수열이 됨
-    //n칸의 경우의 수 n-1칸에서 한칸 + n-2칸에서 2칸
+    let d = [0,1,2,3]
     
-    let dp = []
-    dp[1] = 1, dp[2] = 2, dp[3] = 3, dp[4] = 5
-    
-    for(let i=5; i<=n; i++){
-        dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+    for(let i=4; i<=n; i+=1){
+        d[i] = (d[i-1] + d[i-2]) % 1234567
     }
-    return dp[n]
+    
+    return d[n]
 }
