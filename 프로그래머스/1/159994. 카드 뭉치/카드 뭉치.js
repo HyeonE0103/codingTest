f891a418994ecb81673ae666b1c8b41ec80c63b4
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-05-13 00:36:11 +0900

    [level 1] Title: 카드 뭉치, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,12 +1,11 @@
 function solution(cards1, cards2, goal) {
-    cards1.reverse()
-    cards2.reverse()
+    let a=0, b=0
     
     for(let i=0; i<goal.length; i+=1){
-        if(cards1[cards1.length-1] === goal[i]) cards1.pop()
-        else if(cards2[cards2.length-1] === goal[i]) cards2.pop()
-        else return "No"
+        if(cards1[a] === goal[i]) a+=1
+        else if(cards2[b] === goal[i]) b+=1
+        else return 'No'
     }
     
-    return "Yes"
+    return 'Yes'
 }
