a9bcd514636d9a3e7bd17324bb43224bc6d3a019
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-05-12 23:23:21 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 83.52 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,13 +1,16 @@
 function solution(elements) {
-    const dElements = [...elements, ...elements]
-    let numSet = new Set()
+    const circular = elements.concat(elements)
+    //작은 배열은 spread가 큰 크기의 배열 합치기는 concat이 효율이 좋다는 레퍼런스 참고
+    //큰 배열일 경우 spread를 이용할시 메모리 부족으로 오류 발생할 수 있음
+    let sequence = new Set()
     
     for(let i=0; i<elements.length; i+=1){
         let sum = 0
-        for(let j=i; j<elements.length+i; j+=1){
-            sum += dElements[j]
-            numSet.add(sum)
+        for(let j=0; j<elements.length; j+=1){
+            sum += circular[i+j]
+            sequence.add(sum)
         }
     }
-    return numSet.size
+    
+    return sequence.size
 }
