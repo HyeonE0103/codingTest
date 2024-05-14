210262737b8180acac80836067a0444a2f336586
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-05-15 00:54:02 +0900

    [level 2] Title: 할인 행사, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,24 +1,17 @@
 function solution(want, number, discount) {
-    let wantMap = new Map()
     let result = 0
     
-    for(let i=0; i<want.length; i+=1) wantMap.set(want[i], number[i])
-    
-    for(let i=0; i<discount.length-9; i+=1){
-        const slice = discount.slice(i, i+11)
-        let needs = new Map(wantMap)
+    for(let i=0; i<discount.length - 9; i+=1){
+        const slice = discount.slice(i, i+10);
         let flag = true
         
-        for(let j=0; j<10; j+=1){
-            if(!needs.has(slice[j]) || needs.get(slice[j]) < 1){
+        for(let j=0; j<want.length; j+=1){
+            if(slice.filter(item => item === want[j]).length !== number[j]){
                 flag = false
-                break 
+                break
             }
-            else needs.set(slice[j], needs.get(slice[j]) - 1)
         }
-        
         if(flag) result += 1
     }
-    
     return result
 }
