f9bdb11c46b9626553f49dda476a042cfee1a4de
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-05-20 20:42:19 +0900

    [level 1] Title: 폰켓몬, Time: 0.34 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,6 @@
 function solution(nums) {
-    const half = nums.length/2
-    const max = [...new Set(nums)].length
+    const half = nums.length / 2
+    const max = new Set(nums).size
     
     return max <= half ? max : half
 }
