3957ae06d35ccf7066bc4d7c13049f64ba26ccdb
HyeonE <56420106+HyeonE0103@users.noreply.github.com>
2024-05-22 16:53:09 +0900

    [level 2] Title: H-Index, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,18 +1,5 @@
 function solution(citations) {
-    let arr = []
-    citations.sort((a,b) => b-a)
+    const hIndex = citations.sort((a,b)=>b-a).map((n,i) => n <= i ? n : i+1)
     
-    for(let i=0; i<citations.length; i++) arr.push(Math.min(citations[i], i+1))
-
-    return Math.max(...arr)
+    return Math.max(...hIndex)
 }
-
-/* H-index란 정확히 무엇인지에 대해 알아야 하는데 a번 이상 인용된 논문이 b편 이상이면 a와 b중 작은 값이 hIndex
-
-만약 [9, 7, 6, 2, 1] 의 인용횟수를 가진 교수가 있다면
-9번 이상 인용 횟수를 가진 논문은 1편 이때 H-Index는 1
-7번 이상 인용 횟수를 가진 논문은 2편 이때 H-Index는 2
-6번 이상 인용 횟수를 가진 논문은 3편 이때 H-Index는 3
-2번 이상 인용 횟수를 가진 논문은 4편 이때 H-Index는 2
-1번 이상 인용 횟수를 가진 논문은 5편 이때 H-Index는 1
-*/
